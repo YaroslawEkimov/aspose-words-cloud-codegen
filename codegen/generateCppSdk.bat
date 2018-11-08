5adc52565d8845f5b5457a56983be7669ff080dd
Evgeny Kuleshov <evgeny.kuleshov@aspose.com>
2018-11-08 17:06:57 +0300

    FIxed some issues in templates, updated build script. 2.3.1 swagger-version

--- codegen/generateCppSdk.bat
+++ codegen/generateCppSdk.bat
@@ -1,2 +1,6 @@
 del /S /Q "c:\tmp\cpp" || goto :error
-java -jar codegen\swagger-codegen-cli-2.3.0.jar generate -i spec\asposeforcloud_word.json -l cpprest -t codegen\Templates\cpp -o c:/tmp/cpp/ -c codegen\config.json || goto :error
+java -jar codegen\swagger-codegen-cli.jar generate -i spec\asposeforcloud_word.json -l cpprest -t codegen\Templates\cpp -o c:/tmp/cpp/ -c codegen\config.json || goto :error
+
+del /S /Q "SDKs\cpp\sources" || goto :error
+
+xcopy "c:\tmp\cpp" "SDKs\cpp\sources" /E || goto :error
